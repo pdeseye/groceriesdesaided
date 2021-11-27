eb7340d594da7e16462fba0a9dbfa0324d08bc30
Payal Desai <payal@idesai.com>
2021-11-27 12:18:23 -0500

    edit and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
 
   path('', views.home, name='home'),
   path('lists/', views.lists, name='lists'),
-  path('lists/<int:list_id>/', views.list, name='list')
+  path('lists/<int:list_id>/', views.list, name='list'),
+  path('lists/create/', views.ListCreate.as_view(), name='lists_create'),
+  path('lists/<int:pk>/update/', views.ListUpdate.as_view(), name='lists_update'),
+path('lists/<int:pk>/delete/', views.ListDelete.as_view(), name='lists_delete'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
 from .models import List, Item
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Add the following import
 from django.http import HttpResponse
 
-
-
-items = [
-  Item('1', 'Beer'),
-  Item('1', 'Wine')
-]
-
 # Define the home view
 def home(request):
   return render(request, 'home.html')
+
 def lists(request):
   lists = List.objects.all()#.filter(user=request.user)
-
   return render(request, 'lists.html', { 'lists': lists })
+
 def list(request, list_id):
+  items = Item.objects.all()#.filter(list=request.list)
   return render(request, 'list/index.html', { 'items': items})
+
+class ListCreate(CreateView):
+  model = List
+  fields = '__all__'
+  success_url = '/lists/'
+
+class ListUpdate(UpdateView):
+  model = List
+  fields = '__all__'
+
+class ListDelete(DeleteView):
+  model = List
+  success_url = '/lists/'
