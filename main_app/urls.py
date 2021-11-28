a732d605a9e07a21794c9159cf5c9bc84fdbea96
Payal Desai <payal@idesai.com>
2021-11-28 14:00:17 -0500

    indentation of code

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
   path('lists/<int:list_id>/', views.lists_detail, name='lists_detail'),
   path('lists/create/', views.ListCreate.as_view(), name='lists_create'),
   path('lists/<int:pk>/update/', views.ListUpdate.as_view(), name='lists_update'),
-path('lists/<int:pk>/delete/', views.ListDelete.as_view(), name='lists_delete'),
+  path('lists/<int:pk>/delete/', views.ListDelete.as_view(), name='lists_delete'),
 ]
 
