8ca05348d946322c6996fd0b3857b53a46c749d9
Payal Desai <payal@idesai.com>
2021-11-28 08:56:55 -0500

    update and delete features readded

--- main_app/views.py
+++ main_app/views.py
@@ -22,3 +22,10 @@ class ListCreate(CreateView):
   fields = '__all__'
   success_url = '/lists/'
 
+class ListUpdate(UpdateView):
+  model = List
+  fields = '__all__'
+
+class ListDelete(DeleteView):
+  model = List
+  success_url = '/lists/'
